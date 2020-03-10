19e3f118f4ce514917248ba5446a7fed81729f9e
Cris Oliveira <42254248+engcrisoliveira@users.noreply.github.com>
2020-03-10 12:26:53 -0300

    Add files via upload

---
+++ ex5a.sql
@@ -0,0 +1,5 @@
+#Itens de pedido que se referem ao produto 403
+SELECT codigo_produto, quantidade FROM item_pedidos WHERE codigo_produto = 403
+
+
+
---
+++ ex5b.sql
@@ -0,0 +1,2 @@
+#Pedidos que possuem pelo menos um item com a quantidade maior que 1
+SELECT quantidade FROM item_pedidos WHERE quantidade > 1
