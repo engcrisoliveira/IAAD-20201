578b42dcd1a89db081ce972f156313a224c2116e
Cris Oliveira <42254248+engcrisoliveira@users.noreply.github.com>
2020-03-10 12:28:08 -0300

    Add files via upload

---
+++ ex5f.sql
@@ -0,0 +1,2 @@
+#A soma das quantidades de produtos solicitados nos itens de pedido
+SELECT SUM(quantidade) AS total FROM item_pedidos;
---
+++ ex5g.sql
@@ -0,0 +1,2 @@
+#A média das quantidades de produtos solicitados nos itens de pedido
+SELECT AVG(quantidade) AS total FROM item_pedidos;
